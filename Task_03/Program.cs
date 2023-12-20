5e619bfe6db713164a1244979bc7829f5da5676f
nataliya <vasilenkonatalia02@gmail.com>
2023-12-21 00:29:56 +0300

    Добавили решение в Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+ static void Print2(object[] arr)
+    {
+     if (arr.Length == 0) return;
+        else
+        {
+            Console.Write(" " + arr[0]);
+            Print2(arr.Skip(1).ToArray());
+        }
+      
+    }
+void PrintNumbers(int M, int N)
+{
+if(M > N) return;
+PrintNumbers(M + 1, N);
+Console.Write(M + " ");
+}
+PrintNumbers(1,7);
