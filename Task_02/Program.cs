2b1c9bcd2b71c90663a943aaf3bf9a2e6074cee9
nataliya <vasilenkonatalia02@gmail.com>
2023-12-21 18:44:47 +0300

    Добавили решение в Task_02

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -2,4 +2,24 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+ int A (int m, int n) 
+    
+    {
+             if (m == 0) 
+        {
+            return n + 1;
+        }
+        else if (n == 0 && m > 0)
+         {
+            return A(m - 1, 1);
+        } 
+        else 
+        {
+            return A(m - 1, A(m, n - 1));
+        }
+    }
+int result =  Convert.ToInt32(A(2, 3));
+Console.WriteLine ("Результат: " + result); 
+result = A (3, 2);
+Console.WriteLine ("Результат: " + result); 
 
